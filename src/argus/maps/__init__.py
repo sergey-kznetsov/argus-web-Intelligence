80b9a3eb5fc66f3b2b99a9a481eb738b7854a241
sergey-kznetsov <123492323+sergey-kznetsov@users.noreply.github.com>
2026-08-21 22:28:55 +0800

    Add map provider foundation package

---
+++ src/argus/maps/__init__.py
@@ -0,0 +1,15 @@
+from argus.maps.contracts import (
+    MapPlace,
+    MapProviderCapabilities,
+    MapSearchRequest,
+    MapSearchResult,
+)
+from argus.maps.registry import MapProviderRegistry
+
+__all__ = [
+    "MapPlace",
+    "MapProviderCapabilities",
+    "MapProviderRegistry",
+    "MapSearchRequest",
+    "MapSearchResult",
+]
