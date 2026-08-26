298ab8e231bda531aa5ea331c5044596004d8f88
sergey-kznetsov <123492323+sergey-kznetsov@users.noreply.github.com>
2026-08-26 21:46:54 +0800

    Test public map search versus entity identity

--- tests/test_public_map_review_urls.py
+++ tests/test_public_map_review_urls.py
@@ -1,4 +1,7 @@
-from argus.normalization.public_map_provenance import preferred_public_map_review_url
+from argus.normalization.public_map_provenance import (
+    preferred_public_map_review_url,
+    public_map_surface_kind,
+)
 
 
 def test_yandex_card_resolves_to_public_reviews_view_and_drops_write_review_query():
@@ -28,6 +31,28 @@ def test_google_place_url_is_not_guessed_into_review_uri():
     ) is None
 
 
+def test_map_search_surfaces_are_distinct_from_entity_cards():
+    assert public_map_surface_kind(
+        "https://2gis.ru/perm/search/%D0%9A%D0%BE%D0%BC%D1%81%D0%BE%D0%BC%D0%BE%D0%BB%D1%8C%D1%81%D0%BA%D0%B8%D0%B9%2027"
+    ) == "search"
+    assert public_map_surface_kind(
+        "https://yandex.ru/maps/?text=%D0%9F%D0%B5%D1%80%D0%BC%D1%8C"
+    ) == "search"
+    assert public_map_surface_kind(
+        "https://www.google.com/maps/search/hotels/@58.0,56.2,12z"
+    ) == "search"
+
+    assert public_map_surface_kind(
+        "https://2gis.ru/perm/firm/70000001000000000"
+    ) == "entity"
+    assert public_map_surface_kind(
+        "https://yandex.ru/maps/org/prikamye/123456789/reviews/"
+    ) == "entity"
+    assert public_map_surface_kind(
+        "https://www.google.com/maps/place/Prikamye/data=!4m2!3m1!1s0x123"
+    ) == "entity"
+
+
 def test_lookalike_and_malformed_card_urls_are_not_rewritten():
     assert preferred_public_map_review_url(
         "https://yandex.ru.evil.test/maps/org/company/123456/reviews/"
@@ -35,3 +60,4 @@ def test_lookalike_and_malformed_card_urls_are_not_rewritten():
     assert preferred_public_map_review_url(
         "https://2gis.ru/moscow/firm/not-a-public-id"
     ) is None
+    assert public_map_surface_kind("https://2gis.ru.evil.test/perm/search/example") is None
